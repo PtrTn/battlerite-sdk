8f2f70453574be82199443f1a8fe65cd4fe84b7e
Peter Ton <mail@peterton.nl>
2017-12-01 13:04:49 +0100

    Add integration test

---
+++ tests/Integration/ClientTestTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tests\Integration;
+
+class ClientTestTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldRetrieveMatchesData()
+    {
+        $client = new \PtrTn\Battlerite\Client(
+            new \GuzzleHttp\Client(),
+            getenv('APIKEY')
+        );
+        $matches = $client->getMatches();
+        $this->assertEquals('QUICK2V2', $matches->matches[0]->map->type);
+    }
+}
