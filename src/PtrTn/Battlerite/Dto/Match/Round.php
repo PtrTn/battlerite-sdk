345229641fe1e91b4c169f52340d816d218ad915
Peter Ton <mail@peterton.nl>
2017-12-08 12:55:40 +0100

    Add extra data to detailed match rounds object

--- src/PtrTn/Battlerite/Dto/Match/Round.php
+++ src/PtrTn/Battlerite/Dto/Match/Round.php
@@ -10,27 +10,55 @@ class Round
      * @var string
      */
     public $type;
+
     /**
      * @var string
      */
     public $id;
 
+    /**
+     * @var int
+     */
+    private $duration;
+
+    /**
+     * @var int
+     */
+    private $ordinal;
+
+    /**
+     * @var int
+     */
+    private $winningTeam;
+
     private function __construct(
         string $type,
-        string $id
+        string $id,
+        int $duration,
+        int $ordinal,
+        int $winningTeam
     ) {
         $this->type = $type;
         $this->id = $id;
+        $this->duration = $duration;
+        $this->ordinal = $ordinal;
+        $this->winningTeam = $winningTeam;
     }
 
     public static function createFromArray(array $round): self
     {
         Assert::string($round['type']);
         Assert::string($round['id']);
+        Assert::integer($round['attributes']['duration']);
+        Assert::integer($round['attributes']['ordinal']);
+        Assert::integer($round['attributes']['stats']['winningTeam']);
 
         return new self(
             $round['type'],
-            $round['id']
+            $round['id'],
+            $round['attributes']['duration'],
+            $round['attributes']['ordinal'],
+            $round['attributes']['stats']['winningTeam']
         );
     }
 }
