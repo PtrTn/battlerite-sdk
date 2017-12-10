c1ab1ca752c19ba4a56d863572454de671fe4b5c
Peter Ton <mail@peterton.nl>
2017-12-10 14:24:57 +0100

    Implement caching layer for player endpoint

--- src/PtrTn/Battlerite/ClientWithCache.php
+++ src/PtrTn/Battlerite/ClientWithCache.php
@@ -2,26 +2,32 @@
 
 namespace PtrTn\Battlerite;
 
+use Doctrine\Common\Cache\Cache;
+use Doctrine\Common\Cache\FilesystemCache;
 use GuzzleHttp\Client as GuzzleClient;
-use PtrTn\Battlerite\Dto\Match\DetailedMatch;
-use PtrTn\Battlerite\Dto\Matches\Matches;
 use PtrTn\Battlerite\Dto\Player\DetailedPlayer;
-use PtrTn\Battlerite\Dto\Players\Players;
-use PtrTn\Battlerite\Dto\Status\Status;
-use PtrTn\Battlerite\Query\MatchesQuery;
-use PtrTn\Battlerite\Query\PlayersQuery;
 
 class ClientWithCache
 {
+    private const CACHE_DIR = '/tmp/battlerite';
+
+    private const CACHE_PREFIX = 'battlerite-';
+
+    private const CACHE_LIFETIME = 300;
 
     /**
      * @var Client
      */
     private $client;
+    /**
+     * @var Cache
+     */
+    private $cache;
 
-    public function __construct(Client $client)
+    public function __construct(Client $client, Cache $cache)
     {
         $this->client = $client;
+        $this->cache = $cache;
     }
 
     /**
@@ -35,32 +41,19 @@ class ClientWithCache
                     $apiKey,
                     new GuzzleClient()
                 )
-            )
+            ),
+            new FilesystemCache(self::CACHE_DIR, '.cache')
         );
-    }
-
-    public function getStatus(): Status
-    {
-        return $this->client->getStatus();
-    }
-
-    public function getMatches(MatchesQuery $query = null): Matches
-    {
-        return $this->client->getMatches($query);
-    }
-
-    public function getMatch(string $matchId): DetailedMatch
-    {
-        return $this->client->getMatch($matchId);
-    }
-
-    public function getPlayers(PlayersQuery $query = null): Players
-    {
-        return $this->client->getPlayers($query);
     }
 
     public function getPlayer(string $playerId): DetailedPlayer
     {
-        return $this->client->getPlayer($playerId);
+        $cacheKey = self::CACHE_PREFIX . '-player-' . $playerId;
+        if ($this->cache->contains($cacheKey)) {
+            return $this->cache->fetch($cacheKey);
+        }
+        $data = $this->client->getPlayer($playerId);
+        $this->cache->save($cacheKey, $data, self::CACHE_LIFETIME);
+        return $data;
     }
 }
--- tests/Unit/PtrTn/Battlerite/ClientWithCacheTest.php
+++ tests/Unit/PtrTn/Battlerite/ClientWithCacheTest.php
@@ -1,256 +1,24 @@
 <?php
 namespace Tests\Unit\PtrTn\Battlerite;
 
+use Doctrine\Common\Cache\ArrayCache;
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware;
-use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use PtrTn\Battlerite\ApiClient;
 use PtrTn\Battlerite\Client;
 use PtrTn\Battlerite\ClientWithCache;
-use PtrTn\Battlerite\Dto\Match\DetailedMatch;
-use PtrTn\Battlerite\Dto\Matches\Matches;
 use PtrTn\Battlerite\Dto\Player\DetailedPlayer;
-use PtrTn\Battlerite\Dto\Players\Players;
-use PtrTn\Battlerite\Query\MatchesQuery;
-use PtrTn\Battlerite\Query\PlayersQuery;
 
 class ClientWithCacheTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @test
      */
-    public function shouldRetrieveApiStatus()
-    {
-        $expectedMethod = 'GET';
-        $expectedScheme = 'https';
-        $expectedHost = 'api.dc01.gamelockerapp.com';
-        $expectedPath = '/status';
-
-        $historyContainer = [];
-        $history = Middleware::history($historyContainer);
-        $mockHandler = new MockHandler([
-            new Response(
-                200,
-                [],
-                file_get_contents(__DIR__ . '/fixtures/status-response.json')
-            )
-        ]);
-        $handler = HandlerStack::create($mockHandler);
-        $handler->push($history);
-        $mockClient = new GuzzleClient(['handler' => $handler]);
-        $apiClient = new ClientWithCache(new Client(new ApiClient('fake-api-key', $mockClient)));
-        $apiClient->getStatus();
-
-        $this->assertCount(1, $historyContainer);
-        /** @var Request $request */
-        $request = $historyContainer[0]['request'];
-        $this->assertEquals($expectedMethod, $request->getMethod());
-        $this->assertEquals($expectedScheme, $request->getUri()->getScheme());
-        $this->assertEquals($expectedHost, $request->getUri()->getHost());
-        $this->assertEquals($expectedPath, $request->getUri()->getPath());
-    }
-    /**
-     * @test
-     */
-    public function shouldRetrieveMatches()
-    {
-        $expectedMethod = 'GET';
-        $expectedScheme = 'https';
-        $expectedHost = 'api.dc01.gamelockerapp.com';
-        $expectedPath = '/shards/global/matches';
-
-        $historyContainer = [];
-        $history = Middleware::history($historyContainer);
-        $mockHandler = new MockHandler([
-            new Response(
-                200,
-                [],
-                file_get_contents(__DIR__ . '/fixtures/matches-response.json')
-            )
-        ]);
-        $handler = HandlerStack::create($mockHandler);
-        $handler->push($history);
-        $mockClient = new GuzzleClient(['handler' => $handler]);
-        $apiClient = new ClientWithCache(new Client(new ApiClient('fake-api-key', $mockClient)));
-        $apiClient->getMatches();
-
-        $this->assertCount(1, $historyContainer);
-        /** @var Request $request */
-        $request = $historyContainer[0]['request'];
-        $this->assertEquals($expectedMethod, $request->getMethod());
-        $this->assertEquals($expectedScheme, $request->getUri()->getScheme());
-        $this->assertEquals($expectedHost, $request->getUri()->getHost());
-        $this->assertEquals($expectedPath, $request->getUri()->getPath());
-    }
-
-    /**
-     * @test
-     */
-    public function shouldRetrieveMatchesForQuery()
-    {
-        $expectedMethod = 'GET';
-        $expectedScheme = 'https';
-        $expectedHost = 'api.dc01.gamelockerapp.com';
-        $expectedPath = '/shards/global/matches';
-        $expectedQuery = 'page[offset]=1&page[limit]=6';
-
-        $historyContainer = [];
-        $history = Middleware::history($historyContainer);
-        $mockHandler = new MockHandler([
-            new Response(
-                200,
-                [],
-                file_get_contents(__DIR__ . '/fixtures/matches-response.json')
-            )
-        ]);
-        $handler = HandlerStack::create($mockHandler);
-        $handler->push($history);
-        $mockClient = new GuzzleClient(['handler' => $handler]);
-        $apiClient = new ClientWithCache(new Client(new ApiClient('fake-api-key', $mockClient)));
-
-        $query = MatchesQuery::create()
-            ->withOffset(1)
-            ->withLimit(6)
-        ;
-        $apiClient->getMatches($query);
-
-        $this->assertCount(1, $historyContainer);
-        /** @var Request $request */
-        $request = $historyContainer[0]['request'];
-        $this->assertEquals($expectedMethod, $request->getMethod());
-        $this->assertEquals($expectedScheme, $request->getUri()->getScheme());
-        $this->assertEquals($expectedHost, $request->getUri()->getHost());
-        $this->assertEquals($expectedPath, $request->getUri()->getPath());
-        $this->assertEquals($expectedQuery, urldecode($request->getUri()->getQuery()));
-    }
-
-    /**
-     * @test
-     */
-    public function shouldRetrieveMatch()
-    {
-        $expectedMethod = 'GET';
-        $expectedScheme = 'https';
-        $expectedHost = 'api.dc01.gamelockerapp.com';
-        $expectedPath = '/shards/global/matches/some-match-id';
-        $expectedMatchId = 'AB9C81FABFD748C8A7EC545AA6AF97CC';
-
-        $historyContainer = [];
-        $history = Middleware::history($historyContainer);
-        $mockHandler = new MockHandler([
-            new Response(
-                200,
-                [],
-                file_get_contents(__DIR__ . '/fixtures/match-response.json')
-            )
-        ]);
-        $handler = HandlerStack::create($mockHandler);
-        $handler->push($history);
-        $mockClient = new GuzzleClient(['handler' => $handler]);
-        $apiClient = new ClientWithCache(new Client(new ApiClient('fake-api-key', $mockClient)));
-        $match = $apiClient->getMatch('some-match-id');
-
-        $this->assertCount(1, $historyContainer);
-        /** @var Request $request */
-        $request = $historyContainer[0]['request'];
-        $this->assertEquals($expectedMethod, $request->getMethod());
-        $this->assertEquals($expectedScheme, $request->getUri()->getScheme());
-        $this->assertEquals($expectedHost, $request->getUri()->getHost());
-        $this->assertEquals($expectedPath, $request->getUri()->getPath());
-        $this->assertInstanceOf(DetailedMatch::class, $match);
-        $this->assertEquals($expectedMatchId, $match->id);
-    }
-
-    /**
-     * @test
-     */
-    public function shouldRetrievePlayers()
-    {
-        $expectedMethod = 'GET';
-        $expectedScheme = 'https';
-        $expectedHost = 'api.dc01.gamelockerapp.com';
-        $expectedPath = '/shards/global/players';
-
-        $historyContainer = [];
-        $history = Middleware::history($historyContainer);
-        $mockHandler = new MockHandler([
-            new Response(
-                200,
-                [],
-                file_get_contents(__DIR__ . '/fixtures/players-response.json')
-            )
-        ]);
-        $handler = HandlerStack::create($mockHandler);
-        $handler->push($history);
-        $mockClient = new GuzzleClient(['handler' => $handler]);
-        $apiClient = new ClientWithCache(new Client(new ApiClient('fake-api-key', $mockClient)));
-        $players = $apiClient->getPlayers();
-
-        $this->assertCount(1, $historyContainer);
-        /** @var Request $request */
-        $request = $historyContainer[0]['request'];
-        $this->assertEquals($expectedMethod, $request->getMethod());
-        $this->assertEquals($expectedScheme, $request->getUri()->getScheme());
-        $this->assertEquals($expectedHost, $request->getUri()->getHost());
-        $this->assertEquals($expectedPath, $request->getUri()->getPath());
-        $this->assertInstanceOf(Players::class, $players);
-    }
-
-    /**
-     * @test
-     */
-    public function shouldRetrievePlayersForQuery()
-    {
-        $expectedMethod = 'GET';
-        $expectedScheme = 'https';
-        $expectedHost = 'api.dc01.gamelockerapp.com';
-        $expectedPath = '/shards/global/players';
-        $expectedQuery = 'filter[playerIds]=1234,5678';
-
-        $historyContainer = [];
-        $history = Middleware::history($historyContainer);
-        $mockHandler = new MockHandler([
-            new Response(
-                200,
-                [],
-                file_get_contents(__DIR__ . '/fixtures/players-response.json')
-            )
-        ]);
-        $handler = HandlerStack::create($mockHandler);
-        $handler->push($history);
-        $mockClient = new GuzzleClient(['handler' => $handler]);
-        $apiClient = new ClientWithCache(new Client(new ApiClient('fake-api-key', $mockClient)));
-
-        $query = PlayersQuery::create()
-            ->forPlayerIds([
-                '1234',
-                '5678'
-            ])
-        ;
-        $apiClient->getPlayers($query);
-
-        $this->assertCount(1, $historyContainer);
-        /** @var Request $request */
-        $request = $historyContainer[0]['request'];
-        $this->assertEquals($expectedMethod, $request->getMethod());
-        $this->assertEquals($expectedScheme, $request->getUri()->getScheme());
-        $this->assertEquals($expectedHost, $request->getUri()->getHost());
-        $this->assertEquals($expectedPath, $request->getUri()->getPath());
-        $this->assertEquals($expectedQuery, urldecode($request->getUri()->getQuery()));
-    }
-
-    /**
-     * @test
-     */
     public function shouldRetrievePlayer()
     {
-        $expectedMethod = 'GET';
-        $expectedScheme = 'https';
-        $expectedHost = 'api.dc01.gamelockerapp.com';
-        $expectedPath = '/shards/global/players/some-player-id';
         $expectedPlayerName = 'PlakkeStrasser';
 
         $historyContainer = [];
@@ -265,36 +33,18 @@ class ClientWithCacheTest extends \PHPUnit_Framework_TestCase
         $handler = HandlerStack::create($mockHandler);
         $handler->push($history);
         $mockClient = new GuzzleClient(['handler' => $handler]);
-        $apiClient = new ClientWithCache(new Client(new ApiClient('fake-api-key', $mockClient)));
-        $player = $apiClient->getPlayer('some-player-id');
+        $apiClient = new ClientWithCache(
+            new Client(
+                new ApiClient('fake-api-key', $mockClient)
+            ),
+            new ArrayCache()
+        );
+        $player = $apiClient->getPlayer('same-player-id');
+        $playerFromCache = $apiClient->getPlayer('same-player-id');
 
         $this->assertCount(1, $historyContainer);
-        /** @var Request $request */
-        $request = $historyContainer[0]['request'];
-        $this->assertEquals($expectedMethod, $request->getMethod());
-        $this->assertEquals($expectedScheme, $request->getUri()->getScheme());
-        $this->assertEquals($expectedHost, $request->getUri()->getHost());
-        $this->assertEquals($expectedPath, $request->getUri()->getPath());
         $this->assertInstanceOf(DetailedPlayer::class, $player);
         $this->assertEquals($expectedPlayerName, $player->name);
-    }
-    /**
-     * @test
-     */
-    public function shouldHandleEmptyResponse()
-    {
-        $mockHandler = new MockHandler([
-            new Response(
-                200,
-                [],
-                file_get_contents(__DIR__ . '/fixtures/matches-response-empty.json')
-            )
-        ]);
-        $handler = HandlerStack::create($mockHandler);
-        $mockClient = new GuzzleClient(['handler' => $handler]);
-        $apiClient = new ClientWithCache(new Client(new ApiClient('fake-api-key', $mockClient)));
-        $matches = $apiClient->getMatches();
-
-        $this->assertInstanceOf(Matches::class, $matches);
+        $this->assertEquals($expectedPlayerName, $playerFromCache->name);
     }
 }
