5c1b738eae198398baf270e7f7bb9f4b6ff053c7
Peter Ton <mail@peterton.nl>
2017-12-16 15:57:15 +0100

    Adjust integration test to search players by name

--- tests/Integration/ClientTest.php
+++ tests/Integration/ClientTest.php
@@ -66,19 +66,20 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @test
+     * @group runme
      */
     public function shouldRetrievePlayersData()
     {
-        $playerIds = ['934809523846303744', '934791968557563904'];
+        $playerNames = ['PlakkeStrasser', 'Genaan'];
         $client = \PtrTn\Battlerite\Client::create(getenv('APIKEY'));
         $players = $client->getPlayers(
             PlayersQuery::create()
-            ->forPlayerIds($playerIds)
+            ->forPlayerNames($playerNames)
         );
 
         $this->assertInstanceOf(Players::class, $players);
-        $this->assertEquals('PlakkeStrasser', $players->items[0]->name);
-        $this->assertEquals('Genaan', $players->items[1]->name);
+        $this->assertEquals('934791968557563904', $players->items[0]->id);
+        $this->assertEquals('934809523846303744', $players->items[1]->id);
     }
 
     /**
